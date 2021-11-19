2e7383dc60d033c9b82905db47abc8097498c2dd
flushedpng <93119331+flushedpng@users.noreply.github.com>
2021-11-19 13:56:29 -0500

    Add files via upload

---
+++ Self Learning/main.py
@@ -0,0 +1,634 @@
+# self learning ai that remembers you
+# please, im sleep deprived
+# ~nicolas
+
+import random
+import json
+
+# default states for dictionary saves
+
+skip1 = False
+
+skip = False
+
+# sorta forgot about these
+
+oneanswers = ("Hi there!", "Hey! My name is Azi!")
+secondanswers = ("""I'm Azi, how bout you?
+> """, """I am the ALMIGHTY Azi, and you are..?
+> """)
+thirdanswers = ["I am designed to be a male, so he/him will do I guess.", "he/him", "I am designed to be a guy."]
+
+oompaloompa = True # loop name lol
+
+while oompaloompa == True:
+
+    first = input("> ")
+
+    if first == "hi":
+        print(random.choice(oneanswers))
+    if first == "hello":
+        print(random.choice(oneanswers))
+    if first == "Hi":
+        print(random.choice(oneanswers))
+    if first == "Hello":
+        print(random.choice(oneanswers))
+    if first == "What's your name?":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False # it took me 3 hours to figure this out, get me a coke
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+    if first == "Whats your name?":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "what's your name?":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "whats your name?":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "whats your name":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "What's ur name?":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "whats ur name?":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"} # will check if you have said your pronouns, if not, it will replace the "Pronouns" value to "Syntax Not Found"
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst} # checks true for pronouns
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "whats ur name":
+        secondsecond = input(random.choice(secondanswers))
+        print("Ah,", secondsecond, "nice to meet you!")
+
+        if skip:
+            skip = False
+
+            dict = {"Name":secondsecond, "Pronouns":"Syntax Not Found"}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip:
+                    skip = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip1 = True
+
+
+    if first == "whats my name":
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+        
+    if first == "what's my name":
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+    if first == "What's my name":
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+    if first == "What's my name?":
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+    if first == "whats my name?":
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+    if first == "Whats my name?":
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+    if first == "what's my name?": # checks dict.json for you name
+
+        why = open('dict.json', 'r')
+        why2 = json.load(why)
+
+        print(why2['Name'])
+
+    if first == ("what r ur pronouns"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+    if first == ("what are ur pronouns"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what are your pronouns"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what are your pronouns?"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("What r ur pronouns?"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what r ur pronouns?"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("What are your pronouns?"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what are ur pronouns"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what r your pronouns"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what are ur pronouns?"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+
+    if first == ("what r your pronouns?"):
+        print(random.choice(thirdanswers))
+        firstfirst = input("""What are your pronouns?
+> """)
+        print(firstfirst, "I'll remember that!")
+
+        if skip1:
+            skip1 = False
+
+            dict = {"Name":"Syntax Not Found", "Pronouns":firstfirst} # pronouns value
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+
+                if skip1:
+                    skip1 = True
+
+
+            dict = {"Name":secondsecond, "Pronouns":firstfirst}
+
+            with open('dict.json', 'w') as dictionary:
+                json.dump(dict, dictionary)
+
+        skip = True
+
+    if first == "what are my pronouns":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+        
+    if first == "What are my pronouns":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+
+    if first == "what r my pronouns":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+
+    if first == "What r my pronouns":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+
+    if first == "What are my pronouns?":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+
+    if first == "what are my pronouns?":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+
+    if first == "what r my pronouns?":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
+
+    if first == "What r my pronouns?":
+
+        why3 = open('dict.json', 'r')
+        why4 = json.load(why3)
+
+        print(why4['Pronouns'])
